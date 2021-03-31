3b8f337249edf088a1bbf9054cbb5b0beec8e642
MCC <marieclemence.c@gmail.com>
2021-03-31 15:56:17 +0200

    Mer 31 mar 2021 15:56:16 CEST

--- includes/env.h
+++ includes/env.h
@@ -23,18 +23,10 @@ typedef struct	s_parent_env {
 	char	*shlvl;
 }		t_parent_env;
 
-/*
-typedef	struct	s_env {
-	char		*var;
-	struct s_env	*next;
-}		t_env;
-*/
-
 t_parent_env	ft_parent_env(void);
-//t_env		*add_tail(char *var);
-t_list		*dup_env(char **envp);
-void		add_env_var(t_list **env, char *var);
-char		*get_env_var(char *var, t_list *env);
+t_list			*dup_env(char **envp);
+void			add_env_var(t_list **env, char *var);
+char			*get_env_var(char *var, t_list *env);
 
 
 #endif
--- srcs/builtin/builtin_export.c
+++ srcs/builtin/builtin_export.c
@@ -12,7 +12,6 @@
 
 #include "minishell.h"
 
-
 void    print_export(char **tab_env)
 {
     int     i;
