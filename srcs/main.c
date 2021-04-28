05a84f66719d343a844429a6a574368397051a1c
MCC <marieclemence.c@gmail.com>
2021-04-28 11:27:11 +0200

    Mer 28 avr 2021 11:27:11 CEST

--- srcs/main.c
+++ srcs/main.c
@@ -83,17 +83,17 @@ static char	*fill_line(char *line, t_list *hist, t_list *env)
 char		*read_line(t_list *env, t_list *hist)
 {
 	char	*line;
-	char	*tmp;
+	//char	*tmp;
 
-	tmp = (char *)ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	//tmp = (char *)ft_calloc(BUFFER_SIZE + 1, sizeof(char));
 	line = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!line)
 		printf("ERROR\n"); //à modif
-	line[0] = '\0';
+	//line[0] = '\0';
 	//tmp = ft_strdup(line);
-	free(line);
-	line = fill_line(tmp, hist, env);
-	free(tmp);
+	//free(line);
+	line = fill_line(line, hist, env);
+	//free(tmp);
 	//line = fill_line(line, cmds, hist, env);
 	write(STDOUT_FILENO, "\n", 1);
 	if (!env)
