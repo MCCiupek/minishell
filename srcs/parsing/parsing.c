40e8f5fa7c453c14217cf6e4e2bcab611242e248
MCC <marieclemence.c@gmail.com>
2021-04-29 16:33:14 +0200

    Jeu 29 avr 2021 16:33:13 CEST

--- srcs/parsing/parsing.c
+++ srcs/parsing/parsing.c
@@ -111,7 +111,7 @@ static char **tokenize(char *str, char *sep, t_cmd *c, int redir)
 				{
 					if (c->in)
 						free(c->in);
-					c->in = ft_strtrim(tok, " \t\n<\"\'");
+					c->in = ft_strtrim(tok, " \t<\"\'");
 					if ((fd = open(c->in, O_RDONLY)) < 0)
 					{
 						perror("Couldn't open file");
@@ -126,7 +126,7 @@ static char **tokenize(char *str, char *sep, t_cmd *c, int redir)
 						free(c->out);
 					if (*(tok + 1) == '>')
 						c->out_flags = O_WRONLY|O_CREAT|O_APPEND;
-					c->out = ft_strtrim(tok, " \t\n>\"\'");
+					c->out = ft_strtrim(tok, " \t>\"\'");
 					if ((fd = open(c->out, c->out_flags, 0644)) < 0)
 					{
 						perror("Couldn't open file");
