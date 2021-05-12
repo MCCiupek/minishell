1fde098da9f0cd018e8d063b0dd23a252daad4a5
lkonig <lkonig@student.42.fr>
2021-05-12 11:22:51 +0200

    fix(exit): does not exit when too many arguments

--- includes/builtin.h
+++ includes/builtin.h
@@ -26,5 +26,6 @@ void        built_in_echo(char **cmd, t_list *env);
 void	    builtin_exit(char **builtin, t_list *env, t_list *hist, int silence, int ret);
 void	    builtin_unset(char **builtin, t_list *env);
 int		builtin_export(char **cmd, t_list *env);
+int		array_len(char **a);
 
 #endif
