e4eceda132abf22c1062f5639e75de34e1ac3d4a
user42 <marieclemence.c@gmail.com>
2021-05-17 13:49:20 +0200

    norme(main.c)

--- srcs/main.c
+++ srcs/main.c
@@ -12,7 +12,7 @@
 
 #include "minishell.h"
 
-void		print_prompt(t_list *env)
+static void	print_prompt(t_list *env)
 {
 	char	*tmp[4];
 
@@ -38,45 +38,59 @@ void		print_prompt(t_list *env)
 	free(tmp[2]);
 }
 
+static void	handle_line(char *line, t_list *cmds, t_list *hist, t_list *env)
+{
+	int ret;
+	int err;
+
+	ret = 0;
+	hist = update_hist(line, hist);
+	if ((err = parse_cmd(line, &cmds)))
+		ret = err;
+	else if (!(((t_cmd *)cmds->content)->cmd[0]))
+		ret = 0;
+	else if (ft_strncmp(((t_cmd *)cmds->content)->cmd[0], "exit", 4))
+		ret = exec_cmds(cmds, env, ret, hist);
+	else
+	{
+		free(line);
+		builtin_exit(cmds, env, hist, 0, 0);
+	}
+}
+
+static char	*get_line(int argc, char **argv, t_list *env, t_list *hist)
+{
+	char	*line;
+
+	line = NULL;
+	term_on();
+	if (argc > 2 && !ft_strncmp(argv[1], "-c", 2))
+		line = argv[2];
+	else
+		line = read_line(hist, env);
+	term_off();
+	return (line);
+}
+
 int			main(int argc, char **argv, char **envp)
 {
 	char	*line;
 	t_list	*cmds;
 	t_list	*env;
 	t_list	*hist;
-	int		ret;
-	int		err;
 
 	env = dup_env(envp);
 	set_sig();
 	hist = NULL;
-	ret = 0;
 	while (1)
 	{
 		cmds = NULL;
-		line = NULL;
 		if (argc == 1)
 			print_prompt(env);
-		term_on();
-		if (argc > 2 && !ft_strncmp(argv[1], "-c", 2))
-			line = argv[2];
-		else
-			line = read_line(hist, env);
-		term_off();
+		line = get_line(argc, argv, env, hist);
 		if (line)
 		{
-			hist = update_hist(line, hist);
-			if ((err = parse_cmd(line, &cmds)))
-				ret = err;
-			else if (!(((t_cmd *)cmds->content)->cmd[0]))
-				ret = 0;
-			else if (ft_strncmp(((t_cmd *)cmds->content)->cmd[0], "exit", 4))
-				ret = exec_cmds(cmds, env, ret, hist);
-			else
-			{
-				free(line);
-				builtin_exit(cmds, env, hist, 0, 0);
-			}
+			handle_line(line, cmds, hist, env);
 			if (argc < 2)
 				free(line);
 			ft_lstclear(&cmds, free_t_cmd);
