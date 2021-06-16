a071920742e92a600a993e8b3809dfa429d2668a
Laura KONIG <lkonig@e3r11p16.42.fr>
2021-06-16 18:21:19 +0200

    fix(export): fix list of valid inputs

--- srcs/builtin/builtin_export.c
+++ srcs/builtin/builtin_export.c
@@ -29,7 +29,8 @@ int	export_check_input(char *input)
 			return (export_print_error(input));
 		else if (input[i] != '=' && input[i] != '_'
 			&& (input[i] != '/' && eq == 1)
-			&& input[i] != ' ')
+			&& input[i] != ' ' && input[i] != '|'
+			&& input[i] != '<' && input[i] != '>')
 		{
 			export_print_error(input);
 			return (0);
